0d759a447658413bb22ec3410a02ebc307cf65bb
lyg945 <lyg945@163.com>
2024-01-13 16:33:33 +0800

    0

--- deploy.sh
+++ deploy.sh
@@ -14,19 +14,19 @@ echo 'liuyonggang.com' > CNAME
 
 #echo 'google.com, pub-1725717718088510, DIRECT, f08c47fec0942fa0' > ads.txt
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:lyg945/lyg945.github.io.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://lyg945:${GITHUB_TOKEN}@github.com/lyg945/lyg945.github.io.git
-  git config --global user.name "lyg945"
-  git config --global user.email "lyg945@163.com"
-fi
+# if [ -z "$GITHUB_TOKEN" ]; then
+#  msg='deploy-----------------'
+#  githubUrl=git@github.com:lyg945/lyg945.github.io.git
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://lyg945:${GITHUB_TOKEN}@github.com/lyg945/lyg945.github.io.git
+#   git config --global user.name "lyg945"
+#   git config --global user.email "lyg945@163.com"
+# fi
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git commit -m "deploy-----------------"
+git push -f git@github.com:lyg945/lyg945.github.io.git master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
@@ -41,5 +41,5 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # git commit -m "${msg}"
 # git push -f $codingUrl master # 推送到coding
 
-cd -
-rm -rf docs/.vuepress/dist
+#cd -
+#rm -rf docs/.vuepress/dist
